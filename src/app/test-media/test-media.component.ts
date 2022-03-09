65fb545b7d2a43132fbe4a0ea18024020bb9fbc7
Muhammed <aapozaid@gmail.com>
2022-03-09 13:12:31 +0200

    FIX audio volume indicator refs #4143

--- src/app/test-media/test-media.component.ts
+++ src/app/test-media/test-media.component.ts
@@ -1,122 +1,139 @@
-import { RoomService, LogService, Stream } from 'hug-angular-lib';
-import { AuthService } from './../auth/auth.service';
-import { OpenViduService } from './../openvidu.service';
-import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
-
+import { RoomService, LogService, Stream } from "hug-angular-lib";
+import { AuthService } from "./../auth/auth.service";
+import { OpenViduService } from "./../openvidu.service";
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  OnDestroy,
+} from "@angular/core";
 
 @Component({
-  selector: 'app-test-media',
-  templateUrl: './test-media.component.html',
-  styleUrls: ['./test-media.component.scss']
+  selector: "app-test-media",
+  templateUrl: "./test-media.component.html",
+  styleUrls: ["./test-media.component.scss"],
 })
 export class TestMediaComponent implements OnInit, OnDestroy {
-
   title: {
-    title: 'Test son & vidéo',
-    icon: 'chat'
+    title: "Test son & vidéo";
+    icon: "chat";
   };
 
   videoDeviceId: string;
   audioDeviceId: string;
-  videoDevices  = [];
-  audioDevices  = [];
-  myCamStream
+  videoDevices = [];
+  audioDevices = [];
+  myCamStream;
   volumeLevel: number = 0;
-  showSpinner
+  showSpinner;
   peerId: string;
-  @ViewChild('videoElement') videoElement: ElementRef;
+  @ViewChild("videoElement") videoElement: ElementRef;
 
   constructor(
     private openviduSev: OpenViduService,
     private authService: AuthService,
     private logger: LogService,
-    private roomService: RoomService,
-
-  ) { }
+    private roomService: RoomService
+  ) {}
 
   ngOnInit() {
     this.peerId = this.authService.currentUserValue.id;
     this.showSpinner = true;
-    navigator.mediaDevices.getUserMedia({audio: true, video: true})
-    .then((stream) => {
-      stream.getTracks().forEach(track => track.stop());
-      this.openviduSev.getDevices().then(devices => {
-        console.log('devices ', devices);
-        this.videoDevices = devices.filter(device => device.kind === 'videoinput');
-        if (this.videoDevices.length && this.videoDevices[0].deviceId !== '') {
-          this.videoDeviceId = this.videoDevices[0].deviceId;
-        }
-        this.audioDevices = devices.filter(device => device.kind === 'audioinput');
-        if (this.audioDevices.length && this.audioDevices[0].deviceId !== '') {
-          this.audioDeviceId = this.audioDevices[0].deviceId;
-        }
-        this.initMediaTests();
-      });
-    })
-    .catch((error) => {
-      navigator.mediaDevices.getUserMedia({video: true}).then((stream) => {
-        stream.getTracks().forEach(track => track.stop());
-        this.openviduSev.getDevices().then(devices => {
-          this.videoDevices = devices.filter(device => device.kind === 'videoinput');
-          if (this.videoDevices.length && this.videoDevices[0].deviceId !== '') {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true, video: true })
+      .then((stream) => {
+        stream.getTracks().forEach((track) => track.stop());
+        this.openviduSev.getDevices().then((devices) => {
+          console.log("devices ", devices);
+          this.videoDevices = devices.filter(
+            (device) => device.kind === "videoinput"
+          );
+          if (
+            this.videoDevices.length &&
+            this.videoDevices[0].deviceId !== ""
+          ) {
             this.videoDeviceId = this.videoDevices[0].deviceId;
           }
-          this.initMediaTests();
-        });
-      }).catch(() => {});
-      navigator.mediaDevices.getUserMedia({audio: true}).then((stream) => {
-        stream.getTracks().forEach(track => track.stop());
-        this.openviduSev.getDevices().then(devices => {
-          this.audioDevices = devices.filter(device => device.kind === 'audioinput');
-          if (this.audioDevices.length && this.audioDevices[0].deviceId !== '') {
+          this.audioDevices = devices.filter(
+            (device) => device.kind === "audioinput"
+          );
+          if (
+            this.audioDevices.length &&
+            this.audioDevices[0].deviceId !== ""
+          ) {
             this.audioDeviceId = this.audioDevices[0].deviceId;
           }
           this.initMediaTests();
         });
-      }).catch(() => {});
-    });
-
+      })
+      .catch((error) => {
+        navigator.mediaDevices
+          .getUserMedia({ video: true })
+          .then((stream) => {
+            stream.getTracks().forEach((track) => track.stop());
+            this.openviduSev.getDevices().then((devices) => {
+              this.videoDevices = devices.filter(
+                (device) => device.kind === "videoinput"
+              );
+              if (
+                this.videoDevices.length &&
+                this.videoDevices[0].deviceId !== ""
+              ) {
+                this.videoDeviceId = this.videoDevices[0].deviceId;
+              }
+              this.initMediaTests();
+            });
+          })
+          .catch(() => {});
+        navigator.mediaDevices
+          .getUserMedia({ audio: true })
+          .then((stream) => {
+            stream.getTracks().forEach((track) => track.stop());
+            this.openviduSev.getDevices().then((devices) => {
+              this.audioDevices = devices.filter(
+                (device) => device.kind === "audioinput"
+              );
+              if (
+                this.audioDevices.length &&
+                this.audioDevices[0].deviceId !== ""
+              ) {
+                this.audioDeviceId = this.audioDevices[0].deviceId;
+              }
+              this.initMediaTests();
+            });
+          })
+          .catch(() => {});
+      });
   }
 
   ngOnDestroy() {
-    this.roomService.close()
+    this.roomService.close();
   }
 
   initMediaTests() {
-    this.roomService.onVolumeChange.subscribe(change => {
+    this.roomService.onVolumeChange.subscribe((change) => {
+      console.log("Volume change ", change);
 
-      console.log('Volume change ', change)
+      this.volumeLevel = change.volume * 1000;
+    });
 
-        this.volumeLevel = change.volume
+    this.openviduSev.getTestToken().then(({ token }) => {
+      this.roomService.init({ peerId: this.peerId });
 
-    })
-
-    this.openviduSev
-    .getTestToken()
-    .then(({ token }) => {
-
-
-      this.roomService.init({peerId: this.peerId})
-
-
-      this.roomService.join({roomId:this.peerId,joinVideo: true, joinAudio:true,token: token  })
+      this.roomService.join({
+        roomId: this.peerId,
+        joinVideo: true,
+        joinAudio: true,
+        token: token,
+      });
 
       this.roomService.onCamProducing.subscribe((stream) => {
+        this.logger.debug("Cam producing ", stream);
+        this.myCamStream = stream;
 
-
-        this.logger.debug('Cam producing ', stream)
-        this.myCamStream = stream
-
-
-        this.showSpinner = false
-
-      })
-
-    })
+        this.showSpinner = false;
+      });
+    });
   }
-
-
-
-
-
 }
