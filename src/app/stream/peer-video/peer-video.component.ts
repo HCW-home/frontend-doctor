f1979392d593a407f883082ef0f09ec5a474168b
Muhammed <aapozaid@gmail.com>
2022-04-04 12:56:58 +0200

    FIX issues with video toggle during call refs #4199

--- src/app/stream/peer-video/peer-video.component.ts
+++ src/app/stream/peer-video/peer-video.component.ts
@@ -1,65 +1,61 @@
-
-import { Subscription } from 'rxjs';
-import { Stream, LogService } from 'hug-angular-lib';
-import { Component, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-
+import { Subscription } from "rxjs";
+import { Stream, LogService } from "hug-angular-lib";
+import {
+  Component,
+  Input,
+  ViewChild,
+  ElementRef,
+  OnDestroy,
+  AfterViewInit,
+  OnChanges,
+} from "@angular/core";
 
 @Component({
-  selector: 'app-peer-video',
-  templateUrl: './peer-video.component.html',
-  styleUrls: ['./peer-video.component.scss']
+  selector: "app-peer-video",
+  templateUrl: "./peer-video.component.html",
+  styleUrls: ["./peer-video.component.scss"],
 })
-
-
-export class PeerVideoComponent implements AfterViewInit {
-
-  @ViewChild('videoElement') elementRef: ElementRef;
+export class PeerVideoComponent implements AfterViewInit, OnDestroy, OnChanges {
+  @ViewChild("videoElement") elementRef: ElementRef;
 
   @Input() stream: Stream;
 
-  subscriptions: Subscription[] = []
-  constructor(private logger: LogService) {
-
-  }
+  subscriptions: Subscription[] = [];
+  constructor(private logger: LogService) {}
 
   ngAfterViewInit() {
-    this.updateVideoSrc()
+    this.updateVideoSrc();
   }
 
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-
-        // this.subscriptions.push(this.stream.onLayerChange.subscribe(() => {
-        //   this.logger.debug('ov-vide onlayerchanged.........')
-        //   this.elementRef.nativeElement.srcObject = this.stream.mediaStream
-        // }))
-
-  }
-
+  // ngOnInit(): void {
+  // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  // Add 'implements OnInit' to the class.
+  // this.subscriptions.push(this.stream.onLayerChange.subscribe(() => {
+  //   this.logger.debug('ov-vide onlayerchanged.........')
+  //   this.elementRef.nativeElement.srcObject = this.stream.mediaStream
+  // }))
+  // }
 
   ngOnDestroy(): void {
-    //Called once, before the instance is destroyed.
-    //Add 'implements OnDestroy' to the class.
-    this.subscriptions.forEach(s=> s.unsubscribe())
-
+    // Called once, before the instance is destroyed.
+    // Add 'implements OnDestroy' to the class.
+    this.subscriptions.forEach((s) => s.unsubscribe());
   }
 
   ngOnChanges(changes) {
-    console.log('Peer video changes ', changes)
+    console.log("Peer video changes ", changes, this.elementRef);
     if (changes.stream) {
-      this.stream = changes.stream.currentValue
+      this.stream = changes.stream.currentValue;
+      this.updateVideoSrc();
     }
-    this.updateVideoSrc()
   }
 
   updateVideoSrc() {
     setTimeout(() => {
-      console.log('elemref', this.elementRef)
-      if (this.elementRef) { this.elementRef.nativeElement.srcObject = this.stream.mediaStream }
-
-    }, 100)
-
+      console.log("elemref", this.elementRef, this.stream);
+      if (this.elementRef) {
+        this.elementRef.nativeElement.srcObject = this.stream.mediaStream;
+      }
+    }, 500);
   }
-
 }
