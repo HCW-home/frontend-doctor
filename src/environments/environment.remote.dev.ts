8a5954b46a6034b0e76d0c666081fc79f0a66e8c
Gor Grigoryan <gorgrigoryan1997@gmail.com>
2023-06-26 13:00:19 +0400

    delete dev and staging env

--- src/environments/environment.remote.dev.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  api: "https://dev-hug-at-home.oniabsis.com/api/v1",
-  host: "https://dev-hug-at-home.oniabsis.com/",
-  iceServerUrls : "turn:turn.hcuge.ch",
-  iceServerUsername : "hugathome",
-  iceServerCredential : "x2gbotORZfEB0DLg",
-  version: "2.0.1-2",
-};
--- src/environments/environment.remote.staging.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  api: "https://staging-hug-at-home.oniabsis.com/api/v1",
-  host: "https://staging-hug-at-home.oniabsis.com/",
-  iceServerUrls : "turn:turn.hcuge.ch",
-  iceServerUsername : "hugathome",
-  iceServerCredential : "x2gbotORZfEB0DLg"
-};
