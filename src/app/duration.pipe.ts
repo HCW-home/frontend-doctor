ec118e75f9fe86a179d61891905bdb512478e6d7
Gor Grigoryan <gorgrigoryan1997@gmail.com>
2023-11-10 10:46:12 +0400

    updated some packages

--- src/app/duration.pipe.ts
+++ src/app/duration.pipe.ts
@@ -1,14 +1,15 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import * as moment from 'moment';
+import { Pipe, PipeTransform } from "@angular/core";
+import { intervalToDuration, formatDuration } from "date-fns";
+import { enUS } from "date-fns/locale";
+
 @Pipe({
-  name: 'duration'
+  name: "duration"
 })
 export class DurationPipe implements PipeTransform {
 
-  transform(duration: any, args?: any): any {
-    if(!duration) return '';
-    moment.locale('fr');
-    return moment.duration(duration).humanize();
+  transform(duration: number, args?: any): any {
+    if(!duration) return "";
+    const durationObject = intervalToDuration({ start: 0, end: duration });
+    return formatDuration(durationObject, { locale: enUS });
   }
-
 }
