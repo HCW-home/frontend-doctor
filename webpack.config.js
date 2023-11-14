990270aa6f9f0985c8342b0eec205e393d3b5343
Olivier Bitsch <olivier.b@iabsis.com>
2023-11-14 12:25:09 +0100

    Split js files to chunk + fix environment

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,38 @@ module.exports = {
     path: path.resolve(__dirname, 'dist/hug-at-home'),
   },
   optimization: {
-    chunkIds: 'natural',
-    concatenateModules: true,
+    // chunkIds: 'natural',
+    // concatenateModules: true,
     minimize: true,
+    runtimeChunk: 'single',
+    splitChunks: {
+      maxSize: 1000000,
+      chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+      // minSize: 20000,
+      // minRemainingSize: 0,
+      // minChunks: 1,
+      // maxAsyncRequests: 30,
+      // maxInitialRequests: 30,
+      // enforceSizeThreshold: 50000,
+      // cacheGroups: {
+      //   defaultVendors: {
+      //     test: /[\\/]node_modules[\\/]/,
+      //     priority: -10,
+      //     reuseExistingChunk: true,
+      //   },
+      //   default: {
+      //     minChunks: 2,
+      //     priority: -20,
+      //     reuseExistingChunk: true,
+      //   },
+      // },
+    },
   },
 };
