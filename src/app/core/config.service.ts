a47240943d173743e7f9f2b6d07f255fbe525846
Gor Grigoryan <gorgrigoryan1997@gmail.com>
2023-11-29 17:30:15 +0400

    refs #6122, Implement Matomo (analytics)

--- src/app/core/config.service.ts
+++ src/app/core/config.service.ts
@@ -18,10 +18,28 @@ export class ConfigService {
    getConfig() {
     return this.http.get<any>(`${environment.api}/config`).toPromise().then(config=>{
       console.log('got config', config)
-      this.config = config
-      this.configSub.next(config)
+      this.config = config;
+      if (config.matomoUrl && config.matomoId)
+        this.initializeMatomo(config.matomoUrl, config.matomoId);
+        this.configSub.next(config)
     });
   }
+
+
+    //Matomo
+    initializeMatomo(url: string, siteId: string): void {
+        // @ts-ignore
+        var _paq = window._paq = window._paq || [];
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u = url;
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', siteId]);
+            var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+            g.async = true; g.src = u+'matomo.js'; s.parentNode.insertBefore(g, s);
+        })();
+    }
 
 
 
