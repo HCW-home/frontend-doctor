1e98da023b8b21a6563d2540aa29d14db594d710
Gor Grigoryan <gorgrigoryan1997@gmail.com>
2024-10-16 17:15:58 +0200

    add pictures in PDF

--- src/app/consultations/consultations.component.ts
+++ src/app/consultations/consultations.component.ts
@@ -23,6 +23,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { ConfigService } from '../core/config.service';
 import { InviteService } from '../core/invite.service';
 import { MessageService } from '../core/message.service';
+import { environment } from '../../environments/environment';
 import { ConsultationService } from '../core/consultation.service';
 import { InviteFormComponent } from '../invite-form/invite-form.component';
 import { InviteLinkComponent } from '../invite-link/invite-link.component';
@@ -95,7 +96,7 @@ export class ConsultationsComponent implements OnInit, OnDestroy {
     private inviteService: InviteService,
     private activatedRoute: ActivatedRoute,
     private breakpointObserver: BreakpointObserver,
-    private consultationService: ConsultationService,
+    private consultationService: ConsultationService
   ) {
     this.titles = [
       {
@@ -189,7 +190,6 @@ export class ConsultationsComponent implements OnInit, OnDestroy {
     };
     const dialogRef = this.dialog.open(InviteFormComponent, {
       id: 'invite_form_dialog',
-      // ineffective?
       width: '500px',
       height: '70%',
       data,
@@ -206,7 +206,7 @@ export class ConsultationsComponent implements OnInit, OnDestroy {
         noText: this.translate.instant('invitations.cancelSendAgain'),
         title: this.translate.instant('invitations.resendConfirmTitle'),
       },
-      autoFocus: false
+      autoFocus: false,
     });
     dialogRef.afterClosed().subscribe(confirm => {
       if (confirm) {
@@ -346,14 +346,58 @@ export class ConsultationsComponent implements OnInit, OnDestroy {
     this.generatePDF(consultation.consultation, consultation.nurse);
   }
 
+  adjustMsg(msg, consultationId) {
+    return new Promise(resolve => {
+      if (msg.type === 'attachment') {
+        const requestUrl = `${environment.api}/consultation/${consultationId}/attachment/${msg.id}`;
+        const user = this.authService.currentUserValue;
+
+        if (msg.mimeType.endsWith('jpeg') || msg.mimeType.endsWith('png')) {
+          fetch(requestUrl, {
+            headers: {
+              'x-access-token': user.token,
+            },
+          })
+            .then(res => res.blob())
+            .then(imageFile => {
+              msg.isImage = true;
+              const reader = new FileReader();
+              reader.onload = () => {
+                msg.attachmentsURL = reader.result;
+              };
+              reader.readAsDataURL(imageFile);
+              resolve(msg);
+            })
+            .catch(err => {
+              msg.attachmentsURL = null;
+              resolve(msg);
+            });
+        } else if (msg.mimeType.startsWith('audio')) {
+          msg.isAudio = true;
+          resolve(msg);
+        } else {
+          msg.attachmentsURL = requestUrl;
+          msg.isFile = true;
+          resolve(msg);
+        }
+      } else {
+        resolve(msg);
+      }
+    });
+  }
+
   generatePDF(data, nurse) {
     this.msgServ
       .getConsultationMessages(data._id || data.id, undefined, true)
-      .subscribe(messages => {
+      .subscribe(async res => {
+        const messages = await Promise.all(
+          res.map(m => this.adjustMsg(m, data._id || data.id))
+        );
         const doc = new jsPDF();
         const pageWidth =
           doc.internal.pageSize.width || doc.internal.pageSize.getWidth();
         const imageUrl = this.configService.config?.logo;
+
         if (imageUrl) {
           doc.addImage(
             imageUrl,
@@ -367,15 +411,14 @@ export class ConsultationsComponent implements OnInit, OnDestroy {
           );
         }
 
-        const getLabelWidth = (text: string) => doc.getTextWidth(text) + 2;
-
+        const getLabelWidth = text => doc.getTextWidth(text) + 2;
         doc.setFont('Helvetica', 'normal', 400);
         doc.setFontSize(22);
         doc.text('Consultation report', 15, 40);
-
         doc.setFontSize(14);
         doc.setTextColor('#464F60');
         doc.text('Patient information', 15, 50);
+
         if (nurse?.firstName) {
           doc.text('Requester information', 108, 50);
         }
@@ -389,14 +432,12 @@ export class ConsultationsComponent implements OnInit, OnDestroy {
         doc.text('Firstname:', 15, 55);
         doc.text('Lastname:', 15, 60);
         doc.text('Gender:', 15, 65);
-
         doc.setFont('Helvetica', 'normal', 400);
         doc.text(`${data.firstName}`, 34, 55);
         doc.text(`${data.lastName}`, 34, 60);
         doc.text(`${data.gender}`, 30, 65);
 
         if (nurse?.firstName) {
-          // Requester Information Column
           doc.setFont('Helvetica', 'normal', 700);
           doc.text(`Firstname:`, 108, 55);
           doc.text(`Lastname:`, 108, 60);
@@ -421,52 +462,54 @@ export class ConsultationsComponent implements OnInit, OnDestroy {
         doc.setFontSize(14);
         doc.setTextColor('#464F60');
         doc.text('Consultation information', 15, 75);
-
         doc.setFontSize(10);
         doc.setTextColor('#000');
         doc.setFont('Helvetica', 'normal', 700);
         doc.text(`Start date/time:`, 15, 80);
         doc.text(`End date/time:`, 15, 85);
         doc.text(`Duration:`, 15, 90);
-
         doc.setFont('Helvetica', 'normal', 400);
         doc.text(
-            `${this.datePipe.transform(data.acceptedAt, 'd MMM yyyy HH:mm')}`,
-            15 + getLabelWidth(`Start date/time:`),
-            80
+          `${this.datePipe.transform(data.acceptedAt, 'd MMM yyyy HH:mm')}`,
+          15 + getLabelWidth(`Start date/time:`),
+          80
         );
         doc.text(
-            `${this.datePipe.transform(data.closedAt, 'd MMM yyyy HH:mm')}`,
-            15 + getLabelWidth(`End date/time:`),
-            85
+          `${this.datePipe.transform(data.closedAt, 'd MMM yyyy HH:mm')}`,
+          15 + getLabelWidth(`End date/time:`),
+          85
         );
         doc.text(
-            `${this.durationPipe.transform(data.createdAt - data.closedAt)}`,
-            15 + getLabelWidth(`Duration:`),
-            90
+          `${this.durationPipe.transform(data.createdAt - data.closedAt)}`,
+          15 + getLabelWidth(`Duration:`),
+          90
         );
 
-        const currentYPosition = 95;
+        let chatYPosition = 125;
         if (data.metadata && Object.keys(data.metadata).length) {
           Object.keys(data.metadata).forEach((key, index) => {
             doc.setFont('Helvetica', 'normal', 700);
-            doc.text(`${key}:`, 15, currentYPosition + index * 5);
-
+            doc.text(`${key}:`, 15, chatYPosition + index * 5);
             const metadataX = 15 + getLabelWidth(`${key}:`);
             doc.setFont('Helvetica', 'normal', 400);
-            doc.text(`${data.metadata[key]}`, metadataX, currentYPosition + index * 5);
+            doc.text(
+              `${data.metadata[key]}`,
+              metadataX,
+              chatYPosition + index * 5
+            );
           });
         }
 
         doc.setFontSize(14);
         doc.setTextColor('#464F60');
-        doc.text('Chat history', 15, currentYPosition + 30);
+        doc.text('Chat history', 15, chatYPosition + 30);
+        chatYPosition += 40;
 
-        let chatYPosition = currentYPosition + 40;
-        messages.forEach(message => {
+        for (const message of messages) {
           doc.setFontSize(10);
           doc.setTextColor('#000');
           doc.setFont('Helvetica', 'normal', 700);
+
           const firstName =
             message.fromUserDetail.role === 'patient'
               ? data?.firstName
@@ -475,13 +518,12 @@ export class ConsultationsComponent implements OnInit, OnDestroy {
             message.fromUserDetail.role === 'patient'
               ? data?.lastName
               : message.fromUserDetail.lastName || '';
-          const { role } = message.fromUserDetail;
           const date = this.datePipe.transform(
             message.createdAt,
             'dd LLL HH:mm'
           );
           doc.text(
-            `${firstName} ${lastName} (${role}) - ${date}:`,
+            `${firstName} ${lastName} (${message.fromUserDetail?.role}) - ${date}:`,
             15,
             chatYPosition
           );
@@ -489,13 +531,50 @@ export class ConsultationsComponent implements OnInit, OnDestroy {
           doc.setFont('Helvetica', 'normal', 400);
           doc.setTextColor('#464F60');
           chatYPosition += 5;
-          doc.text(message.text, 15, chatYPosition);
 
-          chatYPosition += 5;
-        });
+          if (message.text) {
+            doc.text(message.text, 15, chatYPosition);
+            chatYPosition += 5;
+          }
+
+          let image = null;
+          if (message.isImage && message.attachmentsURL) {
+            await new Promise<void>(resolve => {
+              image = new Image();
+              image.src = message.attachmentsURL;
+              image.onload = () => {
+                const imgWidth = 50;
+                const imgHeight = (image.height / image.width) * imgWidth;
+                if (
+                  chatYPosition + imgHeight >
+                  doc.internal.pageSize.height - 10
+                ) {
+                  doc.addPage();
+                  chatYPosition = 10;
+                }
+                doc.addImage(
+                  image,
+                  'JPEG',
+                  15,
+                  chatYPosition,
+                  imgWidth,
+                  imgHeight,
+                  `${Math.random()}`,
+                  'FAST'
+                );
+                chatYPosition += imgHeight + 5;
+                URL.revokeObjectURL(message.attachmentsURL);
+                resolve();
+              };
+              image.onerror = () => {
+                URL.revokeObjectURL(message.attachmentsURL);
+                resolve();
+              };
+            });
+          }
+        }
 
         doc.save('consultation-report.pdf');
       });
   }
-
 }
