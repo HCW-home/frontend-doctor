9a002b2da08978cbd776f1489bdddae10ea394f1
Gor Grigoryan <gorgrigoryan1997@gmail.com>
2025-01-10 10:54:47 +0100

    add support for 'onclicl' in footer markdown

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ConfigService } from '../core/config.service';
-import { TranslateService } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-footer',
@@ -8,14 +8,32 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
-  error = false;
   @Input('markdownUrl') markdownUrl = '';
+  processedMarkdown: string = '';
 
-  constructor(
-    public configService: ConfigService
-  ) {
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
+
+  ngOnInit(): void {
+    if (this.markdownUrl) {
+      this.loadMarkdown();
+    }
   }
 
-  ngOnInit() {
+  loadMarkdown(): void {
+    this.http.get(this.markdownUrl, { responseType: 'text' }).subscribe({
+      next: (markdown) => {
+        this.processedMarkdown = this.sanitizeMarkdown(markdown);
+      },
+      error: (err) => {
+      },
+    });
+  }
+
+  sanitizeMarkdown(markdown: string): string {
+    const processedMarkdown = markdown.replace(/onclick="([^"]*)"/g, (match, handler) => {
+      return `onclick="${handler}"`;
+    });
+
+    return this.sanitizer.bypassSecurityTrustHtml(processedMarkdown) as string;
   }
 }
